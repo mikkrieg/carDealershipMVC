3181044adfbe22fee4504074fd163b4a1d0f7fe9
Mikey Kriegel <mikkrieg@gmail.com>
2021-03-02 11:03:43 -0800

    Pass all tests
    Co-authored-by: Jonah Johansen <johansenjonah+git@gmail.com>

---
+++ CarDealership.Solutions/CarDealership.Tests/ModelTests/CarTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using CarDealership.Models;
+
+namespace CarDealership.Tests
+{
+  [TestClass]
+  public class CarTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Car.ClearAll();
+    }
+
+    [TestMethod]
+    public void CarConstructor_CreatesInstanceOfCar_Car()
+    {
+      Car newCar = new Car();
+      Assert.AreEqual(newCar.GetType(), typeof(Car));
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_CarList()
+    {
+      List<Car> newList = new List<Car> {};
+
+      List<Car> result = Car.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsCars_CarList()
+    {
+        Car newCar1 = new Car();
+        Car newCar2 = new Car();
+        List<Car> newCarList = new List<Car>{newCar1, newCar2};
+        List<Car> result = Car.GetAll();
+        CollectionAssert.AreEqual(result, newCarList); 
+    }
+  }
+}
